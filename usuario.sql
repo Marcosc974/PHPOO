f4fa990338142218a191e5b770d166bf45915d12
Marcos Cordeiro <36710037+Marcosc974@users.noreply.github.com>
2018-07-02 09:49:22 -0300

    Update usuario.sql

--- usuario.sql
+++ usuario.sql
@@ -1,61 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 02-Jul-2018 às 01:51
--- Versão do servidor: 5.7.17
--- PHP Version: 5.6.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `classe`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
+-- NOME DO BANCO CLASSE
 
 CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY,
   `nome` varchar(255) CHARACTER SET utf8 NOT NULL,
   `login` varchar(15) CHARACTER SET utf8 NOT NULL,
   `senha` varchar(15) CHARACTER SET utf8 NOT NULL,
   `status` tinyint(1) DEFAULT '1'
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
